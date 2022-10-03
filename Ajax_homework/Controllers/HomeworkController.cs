8aaefe0b4f581dfba9b8cb736367a9cdc77644d1
attact12 <harry80011@gmail.com>
2022-10-03 17:48:39 +0800

    加入專案檔案。

---
+++ Ajax_homework/Controllers/HomeworkController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ajax_homework.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
