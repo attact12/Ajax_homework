a253030f77361c6e3ba9d22d8c33b0c4cbdc3b94
attact12 <harry80011@gmail.com>
2022-10-05 18:24:06 +0800

    10/05 作業完成

--- Ajax_homework/Controllers/HomeworkController.cs
+++ Ajax_homework/Controllers/HomeworkController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ajax_homework.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +12,81 @@ namespace Ajax_homework.Controllers
 {
     public class HomeworkController : Controller
     {
+        private readonly IWebHostEnvironment _host;
+        private readonly DemoContext _context;
+        public HomeworkController(IWebHostEnvironment host, DemoContext context)
+        {
+            _host = host;
+            _context = context;
+        }
         public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult GetDemo()
+        {
+            return View();
+        }
+        public IActionResult demo(string keyword)
+        {
+            if (String.IsNullOrEmpty(keyword))
+            {
+                keyword = "ajax";
+            }
+            return Content($"{keyword}，您好歡迎", "text/html", System.Text.Encoding.UTF8);
+        }
+        public IActionResult Ajevent()
+        {
+            return View();
+        }
+        public IActionResult sleep()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return Content("hello ajaxEvent","text/plain");
+        }
+        public IActionResult Register(Member member, IFormFile File1)
+        {
+            string filePath = Path.Combine(_host.WebRootPath, "uploads", File1.FileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                File1.CopyTo(fileStream);
+            }
+            byte[] imgByte = null;
+            using (var memoryStream = new MemoryStream())
+            {
+                File1.CopyTo(memoryStream);
+                imgByte = memoryStream.ToArray();
+            }
+            member.FileName = File1.FileName;
+            member.FileData = imgByte;
+
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            //string info = $"{file.FileName}-{file.Length}-{file.ContentType}";
+            //string info = _host.ContentRootPath;
+            return Content(filePath, "text/plain", System.Text.Encoding.UTF8);
+        }
+        public IActionResult City()
+        {
+            var cities = _context.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+        public IActionResult Site(string city)
+        {
+            var sites = _context.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct();
+            return Json(sites);
+        }
+
+
+        //根據鄉鎮區名稱讀取路的資料
+        public IActionResult Road(string site)
+        {
+            var roads = _context.Addresses.Where(a => a.SiteId == site).Select(a => a.Road).Distinct();
+            return Json(roads);
+        }
+        public IActionResult Address()
         {
             return View();
         }
--- Ajax_homework/Startup.cs
+++ Ajax_homework/Startup.cs
@@ -1,6 +1,8 @@
+using Ajax_homework.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,13 @@ namespace Ajax_homework
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddDbContext<DemoContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DemoConnection"));
+            });
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
